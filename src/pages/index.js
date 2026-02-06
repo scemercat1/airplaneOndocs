6227f5b9761a7e97a2e8834629d962c1ee9aea56
scemercat1 <business.tytuslemon@gmail.com>
2026-02-06 19:16:53 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,15 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main style={{ padding: '4rem', textAlign: 'center' }}>
+      <h1>Still Watching</h1>
+      <p>Private Discord moderation & notification bot</p>
+
+      <a href="/docs/intro">
+        <button>Read the Docs</button>
+      </a>
+    </main>
+  );
+}
 
